bf30ff8925652abcad8ea51219a42b4668536111
Otávio Murilo <ilegra.paz@agibank.com.br>
2021-09-25 10:06:25 -0300

    Studing threads: synchronized

--- src/test/java/thread/T04SynchronizedThread.java
+++ src/test/java/thread/T04SynchronizedThread.java
@@ -9,17 +9,62 @@ class AccountOperations implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
-            this.withdrawal(10);
+            this.withdrawalSyncBlock(10);
         }
     }
 
-    private void withdrawal(int amount) {
+    public static synchronized void staticSyncMethod() { // Works with static method
+        synchronized (AccountOperations.class) { // And works in static method using block
+            // Do something
+        }
+    }
+
+    private void withdrawalAsync(int amount) { // Async running
         if (account.getBalance() >= amount) {
             System.out.println(this.getThreadName() + ": Is going to withdrawal");
             account.withdrawal(amount);
             System.out.println(this.getThreadName() + ": Finished the withdrawal");
+
+            this.threadSleep();
+
         } else {
             System.out.println(this.getThreadName() + " Can not withdrawal: do not have sufficiently balance");
+        }
+    }
+
+    private synchronized void withdrawalSyncMethod(int amount) { // Sync threads running , it's using lock
+        if (account.getBalance() >= amount) {
+            System.out.println(this.getThreadName() + ": Is going to withdrawal");
+            account.withdrawal(amount);
+            System.out.println(this.getThreadName() + ": Finished the withdrawal");
+
+            this.threadSleep();
+
+        } else {
+            System.out.println(this.getThreadName() + " Can not withdrawal: do not have sufficiently balance");
+        }
+    }
+
+    private void withdrawalSyncBlock(int amount) { // Sync threads running , it's using lock with sync block
+        synchronized (account) {
+            if (account.getBalance() >= amount) {
+                System.out.println(this.getThreadName() + ": Is going to withdrawal");
+                account.withdrawal(amount);
+                System.out.println(this.getThreadName() + ": Finished the withdrawal");
+
+                this.threadSleep();
+
+            } else {
+                System.out.println(this.getThreadName() + " Can not withdrawal: do not have sufficiently balance");
+            }
+        }
+    }
+
+    private void threadSleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
@@ -32,7 +77,6 @@ class AccountOperations implements Runnable {
 public class T04SynchronizedThread {
 
     public static void main(String[] args) {
-        // Scenario 1: With concurrency problem
         final AccountOperations accountOperations = new AccountOperations();
 
         final Thread t1 = new Thread(accountOperations, "Alice");
@@ -40,8 +84,6 @@ public class T04SynchronizedThread {
 
         t1.start();
         t2.start();
-
-        // Scenario 2:
     }
 
 }
